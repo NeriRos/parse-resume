2ddb5a715cc5360a0c09a9f46ebb3b30c25179af
Neriya Rosner <neri.coder@gmail.com>
2023-09-17 10:22:29 +0300

    classify

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
+from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from pdfminer.high_level import extract_text
 
-llm = ChatOpenAI(openai_api_key="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")
-
-structure = open('example/structure.json', 'r').read().split('\n')
+load_dotenv()
 
 
-def parse_resume(path):
-    return extract_text(path)
+class ResumeParser:
+    def __init__(self, resume_path, *args):
+        self.llm = ChatOpenAI()
+        self.structure = open('example/structure.json', 'r').read().split('\n')
+        self.resume_path = resume_path
 
+    def start(self):
+        text = self.parse_resume(self.resume_path)
+        return self.extract_structure(text)
 
-def extract_structure(text):
-    return llm.predict("""act as a resume parser.
-                       I will give you a desired output structure in json format and a resume. 
-                       And you will give me the parsed resume in json format. 
-                       The output structure is as follows:
-                          """ + '\n'.join(structure) + """
-                          Resume:
-                            """ + text)
+    def parse_resume(self, path):
+        return extract_text(path)
+
+    def extract_structure(self, text):
+        return self.llm.predict(self.build_prompt(text))
+
+    def build_prompt(self, text):
+        return """act as a resume parser.
+                           I will give you a desired output structure in json format and a resume. 
+                           And you will give me the parsed resume in json format. 
+                           The output structure is as follows:
+                              """ + '\n'.join(self.structure) + """
+                              Resume:
+                                """ + text
 
 
 if __name__ == '__main__':
-    resume = parse_resume('example/resume.pdf')
-    print(extract_structure(resume))
+    parsed = ResumeParser('example/resume.pdf').start()
+    print(parsed)
